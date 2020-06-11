0d593369b2d2e42118c2a2aa92e3c0c6a824460d
Maria Komova <komova9mary@gmail.com>
2020-06-11 17:15:06 +0300

    Внесла правки в домашку

--- js/stat.js
+++ js/stat.js
@@ -13,12 +13,12 @@ var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -29,17 +29,35 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, score) {
+window.renderStatistics = function (ctx, players, score) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.fillStyle = '#000';
 
   var maxTime = getMaxElement(score);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + GAP_RECT) * i, CLOUD_Y + Y_NAME);
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + GAP_RECT) * i, CLOUD_Y + Y_RECT, BAR_WIDTH, (BAR_HEIGHT* score[i]) / maxTime);
-    ctx.fillText(Math.round(score[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + GAP_RECT) * i, Y_RECT);
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var saturation = Math.random() * 100;
+      ctx.fillStyle = 'hsla(235, ' + saturation + '%' + ', 50%)';
+    }
+
+    var xBar = CLOUD_X + BAR_WIDTH + (BAR_WIDTH + GAP_RECT) * i;
+    var height = (BAR_HEIGHT * score[i]) / maxTime;
+    var yText = CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP;
+    var yBar = yText - FONT_GAP - height;
+
+    ctx.fillRect(xBar, yBar, BAR_WIDTH, height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(players[i], xBar, yText);
+    ctx.fillText(Math.round(score[i]), xBar, yBar - GAP);
   }
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  var titleX = CLOUD_X + GAP;
+  var titleY = CLOUD_Y + GAP;
+  ctx.fillText('Ура вы победили!', titleX, titleY);
+  ctx.fillText('Список результатов:', titleX, titleY + FONT_GAP);
 };
